21028dc875877ba047a2f03101f8bb4158cb3a4e
cmguo <gcm@ustc.edu>
2020-06-03 10:13:27 +0800

    xxx

--- qmessagedata.cpp
+++ qmessagedata.cpp
@@ -10,13 +10,16 @@ QMessageData::QMessageData()
 QMessageData::QMessageData(QMessageData const & o)
 {
     if (o.copy_) {
+        // if <o> holds original msg instance, make a shared copy
         msg_ = o.msg_ = o.copy_(o.msg_);
         o.ref_(msg_, true);
         o.copy_ = nullptr;
     } else {
         // can't go here
+        // if <o> holds shared copy, continue share
         msg_ = o.msg_;
     }
+    // anyway, we hold shared copy now
     copy_ = nullptr;
     ref_ = o.ref_;
     ref_(msg_, true);
@@ -24,6 +27,7 @@ QMessageData::QMessageData(QMessageData const & o)
 
 QMessageData::~QMessageData()
 {
+    // if we hold shared copy, unref it, we may empty (ref_ == null)
     if (!copy_ && ref_)
         ref_(msg_, false);
 }
