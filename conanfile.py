ee85dd8b28b424fc9e11979436a5925e40eb3d9d
cmguo <gcm@ustc.edu>
2020-10-27 11:24:14 +0800

    Conan is here now!

--- conanfile.py
+++ conanfile.py
@@ -22,5 +22,6 @@ class ConanConfig(QtConanFile):
 
 
 if __name__ == '__main__':
+    conans_tools.remove_cache(package_version=f"{package_name}/{package_version}", user_channel=package_user_channel)
     conans_tools.create(user_channel=package_user_channel)
     conans_tools.upload(package_version=f"{package_name}/{package_version}", user_channel=package_user_channel)
